fc7e0af304fd61729d0d536f0f049e547faae717
Ilya A. Shlyakhovoy <bulgarus@inbox.ru>
2023-12-16 20:07:57 +0300

    first commit

---
+++ contracts/utils/CnBCoinAirdrop.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: Private
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/access/manager/AccessManaged.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "../tokens/CnBCoinToken.sol";
+import "./CnBCoinVesting.sol";
+
+contract CnBCoinAirdrop is AccessManaged, ReentrancyGuard, Pausable {
+    /**
+     * @dev Indicates an error related to the already redeemed amount. Used in redeem.
+     * @param target Address who calls redeem..
+     */
+    error CnBCoinAirdropAlreadyRedeemed(address target);
+
+    /**
+     * @dev Indicates an error related to the wrong tree validation. Used in redeem.
+     * @param target Address who calls redeem.
+     * @param amount Amount redeemed.
+     * @param proof The proof provided.
+     */
+    error CnBCoinAirdropWrongPath(
+        address target,
+        uint256 amount,
+        bytes32[] proof
+    );
+
+    /**
+     * @dev Indicates an error when empty token is provided.
+     */
+    error CnBCoinAirdropEmptyToken();
+
+    event Redeem(address indexed account, uint256 amount);
+    event Vesting(address indexed account, uint256 amount);
+
+    bytes32 public root;
+    CnBCoinToken private token;
+    CnBCoinVesting private vesting;
+    uint256 public redeemAmount;
+    mapping(address => bool) private redeemed;
+
+    constructor(
+        CnBCoinToken _token,
+        CnBCoinVesting _vesting,
+        address initialAuthority,
+        uint256 _amount,
+        bytes32 _root
+    ) AccessManaged(initialAuthority) {
+        if (address(_token) == address(0)) {
+            revert CnBCoinAirdropEmptyToken();
+        }
+        root = _root;
+        token = _token;
+        vesting = _vesting;
+        redeemAmount = _amount;
+    }
+
+    /// @notice Addresses can redeem their tokens.
+    /// @param proof Proof path.
+    function redeem(bytes32[] memory proof) public nonReentrant whenNotPaused {
+        if (redeemed[msg.sender]) {
+            revert CnBCoinAirdropAlreadyRedeemed(msg.sender);
+        }
+        bytes32 leaf = keccak256(
+            bytes.concat(keccak256(abi.encode(msg.sender, redeemAmount)))
+        );
+        if (!MerkleProof.verify(proof, root, leaf)) {
+            revert CnBCoinAirdropWrongPath(msg.sender, redeemAmount, proof);
+        }
+        redeemed[msg.sender] = true;
+        if (address(vesting) == address(0)) {
+            token.transfer(msg.sender, redeemAmount);
+            emit Redeem(msg.sender, redeemAmount);
+            return;
+        }
+        token.transfer(address(vesting), redeemAmount);
+        vesting.distribute(msg.sender, redeemAmount);
+        emit Vesting(msg.sender, redeemAmount);
+    }
+
+    /// @notice Update merkle root
+    /// @param _root Merkle root of the addresses white list.
+    function updateMerkleRoot(bytes32 _root) public restricted whenPaused{
+        root = _root;
+    }
+
+    /// @notice Update token address
+    /// @param _token New token address.
+    function updateToken(CnBCoinToken _token) public restricted whenPaused{
+        if (address(_token) == address(0)) {
+            revert CnBCoinAirdropEmptyToken();
+        }
+        token = _token;
+    }
+
+    /// @notice Update vesting address
+    /// @param _vesting New vesting address.
+    function updateVesting(CnBCoinVesting _vesting) public restricted whenPaused{
+        vesting = _vesting;
+    }
+
+    /// @notice Update redeem amount
+    /// @param _amount The redeemed amount.
+    function updateRedeemAmount(uint256 _amount) public restricted whenPaused{
+        redeemAmount = _amount;
+    }
+
+    /// @notice It cancels the Air Drop availability and sends the tokens to the manager provided address.
+    /// @param _to The receiving address.
+    /// @dev Only manager can perform this transaction. It selfdestructs the contract.
+    function cancelAirDrop(address payable _to) public restricted whenPaused {
+        uint256 contractBalance = token.balanceOf(address(this));
+        token.transfer(_to, contractBalance);
+        selfdestruct(_to);
+    }
+}
