1d725f2f1de21f3cf91917805b2ec18878258903
Ilya A. Shlyakhovoy <bulgarus@inbox.ru>
2023-12-29 11:08:05 +0300

    fix prices, disable bsc temporary

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ const app = createApp(App).use(IonicVue, {
  .use(createWeb3Auth({
   projectId: '899c95d8d634fcecf3b9f7859173a69c',
   chains: [
-      Chains.bsc,
+      // Chains.bsc,
       Chains.mainnet,
       Chains.polygon,
       Chains.avalanche,
