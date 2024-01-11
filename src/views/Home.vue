9cb975223b676a7edaea497fb9be22ae562011ee
Ilya A. Shlyakhovoy <bulgarus@inbox.ru>
2024-01-11 18:00:41 +0300

    add contacts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -253,6 +253,71 @@
                 </ion-list>
               </ion-card-content>
             </ion-card>
+
+
+            <ion-card
+              v-if="!disabled"
+              style="min-height: 300px"
+            > 
+              <ion-card-header>
+                <ion-card-title>Contact us</ion-card-title>
+              </ion-card-header>
+              <ion-card-content style="min-height: 200px; padding-top: 90px">
+                <p>
+                 You can contact us using the following details:<br />
+                  Email:
+                  <a href="mailto:support@clearingandbarterhouse.eu"
+                    >support@clearingandbarterhouse.eu</a
+                  ><br />
+                  Telegram:
+                  <a href="https://t.me/ClearingAndBarterHouse" target="_blank"
+                    >@ClearingAndBarterHouse</a
+                  ><br />
+                  Or fill this form to receive a free call
+                </p>
+
+                <ion-list :inset="true">
+                  <ion-item>
+                    <ion-input
+                      v-model="name"
+                      label="Your name"
+                      placeholder="Enter your name"
+                    ></ion-input>
+                  </ion-item>
+
+                  <ion-item>
+                    <ion-input
+                      v-model="phone"
+                      label="Phone or nickname"
+                      placeholder="Enter your phone or nickname"
+                    ></ion-input>
+                  </ion-item>
+                  <ion-item lines="none">
+                    <ion-segment v-model="service">
+                      <ion-segment-button value="call">
+                        <ion-label>Direct call</ion-label>
+                      </ion-segment-button>
+                      <ion-segment-button value="whatsapp">
+                        <ion-label>Whatsapp</ion-label>
+                      </ion-segment-button>
+                      <ion-segment-button value="viber">
+                        <ion-label>Viber</ion-label>
+                      </ion-segment-button>
+                      <ion-segment-button value="telegram">
+                        <ion-label>Telegram</ion-label>
+                      </ion-segment-button>
+                    </ion-segment></ion-item
+                  >
+                  <ion-item lines="none">
+                    <ion-button @click="contactMe" expand="block" size="default"
+                      >Request a free call</ion-button
+                    >
+                  </ion-item>
+                </ion-list>
+              </ion-card-content>
+            </ion-card>
+
+
           </ion-col>
         </ion-row>
       </ion-grid>
